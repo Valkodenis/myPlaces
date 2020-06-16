b64d2d884561d49a5c2abb5423ec521273c11e87
Denis Valko <valkodenis@gmail.com>
2020-06-16 13:24:28 +0300

    data model new view

---
+++ myPlaces/PlaceModel.swift
@@ -0,0 +1,33 @@
+//
+//  PlaceModel.swift
+//  myPlaces
+//
+//  Created by Денис Валько on 16.06.2020.
+//  Copyright © 2020 Denis Valko. All rights reserved.
+//
+
+import Foundation
+
+struct Place {
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    
+    static let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+    
+    static func getPlacec() -> [Place] {
+        var places = [Place]()
+        
+        for place in restaurantNames {
+            places.append(Place(name: place, location: "Уфа", type: "Ресторан", image: place))
+        }
+        
+        return places
+    }
+}
