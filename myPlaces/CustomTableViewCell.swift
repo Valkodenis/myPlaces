a60afa496114dfeb25b807f17c6fc9dbea24652d
Denis Valko <valkodenis@gmail.com>
2020-06-16 12:14:58 +0300

    custom table view cell

---
+++ myPlaces/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  myPlaces
+//
+//  Created by Денис Валько on 16.06.2020.
+//  Copyright © 2020 Denis Valko. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
